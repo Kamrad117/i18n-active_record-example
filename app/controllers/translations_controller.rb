08b471b475feab71c2ccea28728f2b93326d5961
kamrad117 <kamrad117@gmail.com>
2013-09-11 18:26:40 +0300

    fix for default translations

--- app/controllers/translations_controller.rb
+++ app/controllers/translations_controller.rb
@@ -1,10 +1,10 @@
 class TranslationsController < ApplicationController
   def index
-    @translations = I18n::Backend::ActiveRecord::Translation.locale(:en).all
+    @translations = I18n::Backend::ActiveRecord::Translation.locale(:en).where(user_id: current_user.id)
   end
 
   def create
-    I18n.backend.store_translations(params[:locale], {params[:key] => params[:value]}, current_user.id, :escape => false)
+    TRANSLATIONS_STORE.store_translations(params[:locale], {params[:key] => params[:value].strip}, current_user.id)
     redirect_to translations_url, :notice => "Added translations"
   end
 end
--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -8,7 +8,7 @@
       escape = options.fetch(:escape, true)
       flatten_translations(locale, data, escape, false).each do |key, value|
         Translation.locale(locale).lookup(expand_keys(key)).delete_all
-        Translation.create(:locale => locale.to_s, :key => key.to_s, :value => value, ENV['translation_assoc_field'] => assoc_id )
+        Translation.create(:locale => locale.to_s, :key => key.to_s, :value => value, ENV['translation_assoc_key'].to_sym => assoc_id )
       end
     end
   end
@@ -36,14 +36,6 @@
     end
   end
 
-  I18n.backend = I18n::Backend::ActiveRecord.new
+  TRANSLATIONS_STORE = I18n::Backend::ActiveRecord.new
 
-  # require 'i18n/backend/active_record'
-  # TRANSLATIONS_STORE = I18n::Backend::ActiveRecord.new
-
-  # I18n::Backend::ActiveRecord.send(:include, I18n::Backend::Memoize)
-  # I18n::Backend::ActiveRecord.send(:include, I18n::Backend::Flatten)
-  # I18n::Backend::Simple.send(:include, I18n::Backend::Memoize)
-  # I18n::Backend::Simple.send(:include, I18n::Backend::Pluralization)
-
-  # I18n.backend = I18n::Backend::Chain.new(I18n::Backend::Simple.new, TRANSLATIONS_STORE)
+  I18n.backend = I18n::Backend::Chain.new(TRANSLATIONS_STORE, I18n.backend)
